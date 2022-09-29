40a2eae41d19a6022e3fa51c0e4fd06f88ceb4ee
Yuri Ramos <yroramos@gmail.com>
2022-09-29 16:29:09 -0300

    fale conosco com localstorage

--- js/fale-conosco.js
+++ js/fale-conosco.js
@@ -5,19 +5,31 @@ let formulario ={
     btnEnviar :document.querySelector("#btn-enviar")
 };
 
-formulario.btnEnviar.addEventListener("click",() =>{
+formulario.btnEnviar.addEventListener("click",(e) =>{
     var nome = formulario.nome.value;
     var email = formulario.email.value;
     var mensagem = formulario.mensagem.value;
+    var cont=0;
 
     if(nome.length < 1  ){
         alert("Insira um nome válido!")
-    }
+    }else{cont++}
     if(email.length < 1){
         alert("Insira um email válido!")
-    }
+    }else{cont++}
     
     if(mensagem.length < 1){
         alert("Insira uma mensagem válida!")
+    }else{cont++}
+
+    if (cont!=3){
+        e.preventDefault;
+
+    }else{
+        localStorage.nome=nome
+        localStorage.email=email
+        localStorage.mensagem= mensagem
     }
 })
+
+
